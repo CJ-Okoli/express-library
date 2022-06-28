ab8d9c7a3cce17b0dd267e23495de264ce48c187
CJ-Okoli <67511379+CJ-Okoli@users.noreply.github.com>
2022-06-29 00:07:09 +0100

    Updated the app for Heroku
    Added files and changes required for deployment to Heroku.

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://tutorial:tutorial@cluster0.kvnt1no.mongodb.net/local_library1?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://tutorial:tutorial@cluster0.kvnt1no.mongodb.net/local_library1?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
